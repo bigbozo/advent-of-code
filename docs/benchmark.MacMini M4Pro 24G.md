d7c003314e0ee656a79b2873b5e530d6e222c62f
Bugi Goertz <bugi@joboland.de>
2025-12-05 22:48:33 +0100

    refactored for readability

--- docs/benchmark.MacMini M4Pro 24G.md
+++ docs/benchmark.MacMini M4Pro 24G.md
@@ -1,7 +1,8 @@
     # Benchmark Results
     
      iter |             benchmark |    subject | set |     revs |     mem_peak |   time_avg | comp_z_value | comp_deviation 
-     0.00 | AdventOfCodeBench2025 | benchDay01 |     |   100.00 | 2,078,344.00 |   3,061.07 |      +0.00σ |         +0.00% 
-     0.00 | AdventOfCodeBench2025 | benchDay02 |     | 1,000.00 | 1,788,936.00 |     364.29 |      +0.00σ |         +0.00% 
-     0.00 | AdventOfCodeBench2025 | benchDay03 |     |   100.00 | 2,598,928.00 |   3,007.62 |      +0.00σ |         +0.00% 
-     0.00 | AdventOfCodeBench2025 | benchDay04 |     |     3.00 | 5,301,752.00 | 656,851.33 |      +0.00σ |         +0.00% 
+     0.00 | AdventOfCodeBench2025 | benchDay01 |     |   100.00 | 2,079,472.00 |   3,138.26 |      +0.00σ |         +0.00% 
+     0.00 | AdventOfCodeBench2025 | benchDay02 |     | 1,000.00 | 1,788,936.00 |     371.41 |      +0.00σ |         +0.00% 
+     0.00 | AdventOfCodeBench2025 | benchDay03 |     |   100.00 | 2,600,056.00 |   3,049.86 |      +0.00σ |         +0.00% 
+     0.00 | AdventOfCodeBench2025 | benchDay04 |     |     3.00 | 5,308,160.00 | 532,920.00 |      +0.00σ |         +0.00% 
+     0.00 | AdventOfCodeBench2025 | benchDay05 |     | 1,000.00 | 1,788,936.00 |   7,482.20 |      +0.00σ |         +0.00% 
--- src/Year2025/Day05/Solution.php
+++ src/Year2025/Day05/Solution.php
@@ -2,7 +2,6 @@
 
 namespace Bizbozo\AdventOfCode\Year2025\Day05;
 
-use Bizbozo\AdventOfCode\Ranges\Range;
 use Bizbozo\AdventOfCode\Solutions\SolutionInterface;
 use Bizbozo\AdventOfCode\Solutions\SolutionResult;
 use Bizbozo\AdventOfCode\Solutions\UnitResult;
@@ -11,12 +10,12 @@ use Override;
 class Solution implements SolutionInterface
 {
 
-    private function parseData(string $stream)
+    private function parseData(string $stream): array
     {
-        list($patterns, $numbers) = explode(PHP_EOL . PHP_EOL, $stream);
+        list($ranges, $numbers) = explode(PHP_EOL . PHP_EOL, $stream);
 
         return [
-            array_map(fn($item) => explode('-', $item), explode(PHP_EOL, $patterns)),
+            array_map(fn($item) => explode('-', $item), explode(PHP_EOL, $ranges)),
             explode(PHP_EOL, $numbers)
         ];
     }
@@ -30,40 +29,42 @@ class Solution implements SolutionInterface
     public function solve(string $inputStream, ?string $inputStream2 = null): SolutionResult
     {
 
-        [$patterns, $numbers] = static::parseData($inputStream);
+        [$ranges, $ingredientIds] = static::parseData($inputStream);
 
         $freshIngredients = 0;
-        foreach ($numbers as $number) {
-            $fresh = false;
-            foreach ($patterns as $pattern) {
-                if ($number >= $pattern[0] && $number <= $pattern[1]) {
+        foreach ($ingredientIds as $id) {
+
+            foreach ($ranges as $range) {
+                if ($id >= $range[0] && $id <= $range[1]) {
                     $freshIngredients++;
                     break;
                 }
             }
         }
 
-        usort($patterns, fn($a, $b) => $a[0] <=> $b[0]);
+        usort($ranges, fn($a, $b) => $a[0] <=> $b[0]);
 
-        $right = 0;
-        $total = 0;
-        foreach ($patterns as $pattern) {
+        $rightBound = 0;
+        $numberOfFreshIngredientIds = 0;
 
-            if ($pattern[0] <= $right) {
-                if ($pattern[1] <= $right) {
+        foreach ($ranges as $range) {
+
+            if ($range[0] <= $rightBound) {
+                if ($range[1] <= $rightBound) {
                     continue;
                 }
-                $pattern[0] = $right + 1;
+                $range[0] = $rightBound + 1;
             }
-            $total += $pattern[1] - $pattern[0] + 1;
-            $right = $pattern[1];
+
+            $numberOfFreshIngredientIds += $range[1] - $range[0] + 1;
+            $rightBound = $range[1];
 
         }
 
         return new SolutionResult(
             5,
             new UnitResult("%s ingredients are fresh", [$freshIngredients]),
-            new UnitResult('%s ids are considered fresh', [$total])
+            new UnitResult('%s ids are considered fresh', [$numberOfFreshIngredientIds])
         );
     }
 }
